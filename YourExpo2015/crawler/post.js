284a6b4c07b3459e6f7020c5bb7fb85758b1d024
Andrea <andrea.mauri87@gmail.com>
2014-12-09 12:00:53 +0100

    *added more data to the photo sent to the CS *fixed the delta

--- YourExpo2015/crawler/post.js
+++ YourExpo2015/crawler/post.js
@@ -59,7 +59,10 @@ module.exports = function postToCS(documents) {
     return {
       data: {
         id: data._id,
-        url: data.imageUrl
+        url: data.imageUrl,
+        username: data.username,
+        votesCount: data.votesCount,
+        detla: data.delta
       }
     };
   });
