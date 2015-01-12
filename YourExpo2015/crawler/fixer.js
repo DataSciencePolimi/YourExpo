4bc1623840cc9d380f76401e889f02864d6daafb
Riccardo Volonterio <voloxxx@gmail.com>
2015-01-12 09:49:45 +0100

    Fixed query

--- YourExpo2015/crawler/fixer.js
+++ YourExpo2015/crawler/fixer.js
@@ -37,7 +37,9 @@ function handleTag( tag ) {
   return Model
   .find()
   .where( 'tag', tag )
+  .where( 'liked' ).ne( true )
   .select( '-raw' )
+  .limit( 100 )
   .execAsync()
   .then( actions )
   ;
